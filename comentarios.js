a538f20b756ecf68aadee7f3430b1f2df3e62772
angelo0bonassi <angelo.bonassi@aluno.senai.br>
2026-02-26 12:14:21 -0300

    inicio com js

---
+++ comentarios.js
@@ -0,0 +1,15 @@
+// O comsole é utilizado pra imprimir algo na tela
+
+
+console.log("Sesi");
+// console.log("Senai");
+
+
+/*
+console.log("Senai");
+ console.log("Senai");
+ console.log("Senai");
+ console.log("Senai");
+ console.log("Senai");
+ 
+ */
---
+++ desafio_bloco.js
@@ -0,0 +1,26 @@
+/*
+   Desafio: Criar dois blocos de codigo.
+   Onde o primeiro deve imprimir na tela os 
+   numeros pares de 0 a 10 e o segundo 
+   bloco deve imprimir na tela os impares
+*/
+
+ console.log("2 4 6 8 10");
+ console.log("1 3 5 7 9");
+
+
+
+ {
+    var pares = "0, 2, 4, 6, 8, 10";
+    console.log("Numero pares", pares);
+ }
+
+
+ {
+    var impares = "1, 3, 5, 7, 9";
+    console.log("Numero impares", impares);
+ }
+
+
+
+ 
