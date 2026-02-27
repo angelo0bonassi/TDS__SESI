fe6f4089d8e6e24d8831ffe754648a6f6dd47df8
angelo0bonassi <angelo.bonassi@aluno.senai.br>
2026-02-27 11:47:59 -0300

    inicio com js

---
+++ inicio/dados2.js
@@ -0,0 +1,12 @@
+ //console.log(19.9 * 0.6);
+
+ var preco = 19.9;
+ var desconto = 0.6;
+ var precoComDesconto = preco * (1 - desconto);
+ //console.log(precoComDesconto);
+
+ let nome = "Caderno";
+ let categoria = "Papelaria";
+ console.log("Produto: " + nome + "  Valor: "
+ + preco + "   Categoria: " + categoria +
+ "   Desconto: "+ precoComDesconto);
--- inicio/desafio_bloco.js
+++ inicio/desafio_bloco.js
@@ -5,22 +5,22 @@
    bloco deve imprimir na tela os impares
 */
 
- console.log("2 4 6 8 10");
+ console.log("0 2 4 6 8 10");
  console.log("1 3 5 7 9");
 
 
 
  {
     var pares = "0, 2, 4, 6, 8, 10";
-    console.log("Numero pares", pares);
+    console.log("Numero pares: ", pares);
  }
 
 
  {
     var impares = "1, 3, 5, 7, 9";
-    console.log("Numero impares", impares);
+    console.log("Numero impares: ", impares);
  }
 
 
-
+ 
  
