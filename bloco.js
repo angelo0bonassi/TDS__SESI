2f578d7a5c0308300408640a66536c9b83204aa9
angelo0bonassi <angelo.bonassi@aluno.senai.br>
2026-02-27 10:38:10 -0300

    Add files via upload

---
+++ bloco.js
@@ -0,0 +1,18 @@
+{
+    console.log("Passo 01");
+    console.log("Passo 02");
+}
+{
+    console.log("Passo 01");
+    console.log("Passo 02");
+}
+
+{
+    {
+        {
+
+        }
+    }
+}
+
+console.log("Fim")
