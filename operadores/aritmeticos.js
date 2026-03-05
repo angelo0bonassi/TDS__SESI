eec65238147d20bc7fbe05fa5cfb21bb0bb590d8
angelo0bonassi <angelo.bonassi@aluno.bonassi>
2026-03-05 12:12:39 -0300

    aritimeticos

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,18 @@
+// valores literais
+console.log(10 + 5); //adição
+console.log(10 - 5); //subtração
+console.log(10 * 5); //multiplicação
+console.log(10 / 5); //divisão
+console.log(10 % 5); //resto
+
+
+// valores variaveis
+
+let a = 100;
+let b = 50;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
