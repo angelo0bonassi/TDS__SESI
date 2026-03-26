130d835e5b435ba3291770796819b09276d47d79
angelo0bonassi <angelo.bonassi@aluno.bonassi>
2026-03-26 12:08:13 -0300

    Atividade 3

---
+++ atividades/atividade3.js
@@ -0,0 +1,9 @@
+let usuariaIdade = 19;
+let possuiConvite = true;
+
+if (usuariaIdade >= 18 && possuiConvite == true){
+    console.log("O usuario possui Convite Vip e eh maior de idade, Entrada Permitida");
+}
+else{
+    console.log("O usuario nao possui o vip ou nao eh maior de idade, Entrada Negada");
+}
