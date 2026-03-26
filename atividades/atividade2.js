52cff7cd7067cce2b5e94994afb3769b66ab90b6
angelo0bonassi <angelo.bonassi@aluno.bonassi>
2026-03-26 11:47:33 -0300

    Atividade 2

---
+++ atividades/atividade2.js
@@ -0,0 +1,6 @@
+let idadeCahorro =  9;
+let realIdade = (idadeCahorro * 7);
+
+console.log(`A real idade de um cachorro eh 7 vezes maior que a de um humano.
+Ou seja um cahorro que tem ${idadeCahorro} anos de idade de cachorro
+em anos humanos ele tem ${realIdade} anos de idade`);
