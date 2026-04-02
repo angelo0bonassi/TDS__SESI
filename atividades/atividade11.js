8e629ec786052beb791eec41468e6187f128e882
angelo0bonassi <angelo.bonassi@aluno.bonassi>
2026-04-02 12:13:43 -0300

    Atividades

---
+++ atividades/atividade11.js
@@ -0,0 +1,10 @@
+const nomes = ["angelo", "henrico", "gustavo", "joao", "gutavo graff", "zoio", "alek", "pedro", "miguel", "felipao"];
+
+if(nomes.includes("angelo"))
+{
+    console.log("Usuario localizado no sitema");
+}
+else
+{
+    console.log("Usuario nao localizado");
+}
